f85268287d59c989f90544818c4ea11ebac311b2
Guillaume Sintes <guillaumesintes@orange.fr>
2023-08-01 13:18:08 +0200

    Implement neuralnet

---
+++ mynet/neuralnet.py
@@ -0,0 +1,29 @@
+"""A neural network is a sequence of layers."""
+
+from typing import Sequence, Iterator, Tuple
+
+from mynet.layers import Layer
+from mynet.tensor import Tensor
+
+class NeuralNet:
+    def __init__(self, layers : Sequence[Layer]) -> None:
+        self.layers = layers
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        """Push the input forward."""
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
+    
+    def backward(self, grad: Tensor) -> Tensor:
+        """Backpropagate the gradient through the layers."""
+        for layer in reversed(self.layers):
+            grad = layer.background(grad)
+        return grad
+    
+    def param_and_grads(self) -> Iterator[Tuple[Tensor, Tensor]]:
+        """Return an iterator over the parameters and there gradients for all layers."""
+        for layer in self.layers:
+            for name, param in layer.params.items():
+                grad = layer.grads[name]
+                yield param, grad
